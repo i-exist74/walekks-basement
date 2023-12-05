debab33d9c5e6afa7e559c5e21d3e3ce594da8e5
hootis <81767621+Valshion007@users.noreply.github.com>
2023-12-04 23:55:48 -0800

    added code mod stuff

---
+++ src/PluginMain.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using BepInEx;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TemplateRWMod
+{
+    //[BepInDependency("author.some_other_mods_guid", BepInDependency.DependencyFlags.HardDependency)]
+    // to anyone looking at this, please fix the dependencies. i forgot how to set them up properly lmao
+
+    [BepInPlugin(modID, pluginName, version)]
+    public class PluginMain : BaseUnityPlugin
+    {
+        public const string modID = "walekks.basement";
+        public const string pluginName = "Walekk's Basement";
+        public const string version = "1.0.0";
+
+        public static RemixOptions options;
+
+        private void OnEnable()
+        {
+            try
+            {
+                Logger.LogInfo("loading plugin " + pluginName);
+
+                On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
+                On.RainWorld.OnModsInit += RainWorld_OnModsInit;
+
+                Logger.LogInfo("plugin " + pluginName + " is loaded!");
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+            }
+        }
+
+        private void LoadResources(RainWorld rainWorld)
+        {
+            //obsolete currently
+        }
+
+        private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld rainWorld)
+        {
+            orig(rainWorld);
+            options = new RemixOptions(this); //remix options is just placeholder for if we need them
+            OptionInterface oi = options;
+            try
+            {
+                MachineConnector.SetRegisteredOI(modID, oi);
+
+                //put hooks or whatever here
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+    }
+}
---
+++ src/RemixOptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Menu.Remix;
+using Menu.Remix.MixedUI;
+using RWCustom;
+using UnityEngine;
+
+namespace TemplateRWMod
+{
+    public class RemixOptions : OptionInterface
+    {
+        public RemixOptions(PluginMain pluginMain) 
+        {
+            //put binds here
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            this.Tabs = new OpTab[]
+            {
+                new OpTab(this, "optab")
+            };
+            OpLabel opLabel = new OpLabel(new Vector2(150f, 560f), new Vector2(300f, 30f), "placeholder", FLabelAlignment.Center, true, null);
+            this.Tabs[0].AddItems(new UIelement[]
+            {
+                opLabel
+            });
+        }
+    }
+}
